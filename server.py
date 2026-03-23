d6973e8dfcd656e8cf77f95523fe8835e141fe0d
Kayra <kayracizmeci@gmail.com>
2026-03-23 14:46:16 +0300

    fix: run mode algorithm control parameters

--- server.py
+++ server.py
@@ -108,7 +108,7 @@ def receive_data(data: CalculationReq):
     
             
         if data.alg_mod == 'run':
-            x = algorithm_control(x, steps, operations)
+            x = algorithm_control(data.x, data.steps, data.operations)
             return {'result': x, 'status': 'success'}
         
 
